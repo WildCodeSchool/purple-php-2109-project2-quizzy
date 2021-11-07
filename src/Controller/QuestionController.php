f5d17fd20e6331ca4f6311190538ef9eaa30c668
Morgane Lailler <morgane.lailler.pro@gmail.com>
2021-11-07 19:00:13 +0100

    US 2 - added session to keep id question and answers order

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -11,15 +11,35 @@ class QuestionController extends AbstractController
     public function show(): string
     {
         //Function to send one question and its answers to the view Question/index
+        session_start();
         $questionManager = new QuestionManager();
         $question = $questionManager->selectRandomQuestion();
         $idQuestion = $question['id']; // fetch the id of the question selected
         $answersManager = new AnswersManager();
         $answers = $answersManager->selectAnswersForQuestion($idQuestion);
         shuffle($answers);
+        $_SESSION["question"] = $question;
+        $_SESSION["answers"] = $answers;
         return $this->twig->render('Question/index.html.twig', [
             'question' => $question,
             'answers' => $answers,
+            'session' => $_SESSION,
         ]);
+    }
+
+    public function showResults()
+    {
+        session_start();
+        if (isset($_SESSION['question']) && isset($_SESSION['answers'])) {
+            $question = $_SESSION['question'];
+            $answers = $_SESSION['answers'];
+            return $this->twig->render('Question/result.html.twig', [
+                'question' => $question,
+                'answers' => $answers,
+            ]);
+        } else {
+            header('Location:/');
+            //maybe add a new page error to explain that the website needs cookies to function
+        }
     }
 }
