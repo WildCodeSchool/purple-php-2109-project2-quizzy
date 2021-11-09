ad5042630353983c8c686feea1dc09a67533d57f
Benjamin-51 <babilliot.benjamin@gmail.com>
2021-11-09 18:06:44 +0100

    US4 Improved the form and prepared the controller.
    The form now gets back the information sent if an error occurs, it's also now a loop that either creates two divs or enouth divs to contain the answers. The controller prepares the sent form for the sql query.

--- src/Controller/ManagementController.php
+++ src/Controller/ManagementController.php
@@ -6,7 +6,50 @@ class ManagementController extends AbstractController
 {
     public function showFormAddQuestion(): string
     {
+        $errors = [];
+        $question = "";
+        $answerArray = [];
+        $minOneCorrectAnswer = false;
+        $numberOfAnswers = 1;
+
+        if (!empty($_POST)) { // If a question is being sent, it has to be checked then added.
+            if (isset($_POST["question"])) {
+                $question = $_POST["question"];
+            } else {
+                $errors[] = "Il faut remplir le champ question.";
+            }
+            while (true) { // This pulls the answers from the form and add them to $answerArray
+                if (isset($_POST["answer_" . strval($numberOfAnswers)])) {
+                    $answer = $_POST["answer_" . strval($numberOfAnswers) ];
+                    $isCorrect = isset($_POST["answer_" . strval($numberOfAnswers) . "_correct"]);
+                    $answerArray[] = ["answer" => $answer, "isCorrect" => $isCorrect];
+                    $numberOfAnswers++;
+                } else {
+                    break;
+                }
+            }
+            foreach ($answerArray as $answer) { // This checks if answer forms are filled properly.
+                if (empty($answer["answer"])) {
+                    $errors[] = "Tout les champs réponses doivent être remplis.";
+                }
+                if ($answer["isCorrect"]) { // This checks if at least one answer is correct.
+                    $minOneCorrectAnswer = true;
+                }
+            }
+
+            if ($minOneCorrectAnswer == false) {
+                $errors = "Au moins une réponse doit être marquée comme correcte.";
+                return $this->twig->render('Management/add-question.html.twig', [
+                    'errors' => $errors,
+                    'question' => $question,
+                    'answerArray' => $answerArray,
+                ]);
+            }
+        }
+
         return $this->twig->render('Management/add-question.html.twig', [
-        ]);
+            'question' => "",
+            'answerArray' => [["", false],["", false]],
+            ]);
     }
 }
