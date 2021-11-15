4bb8e992f594a123f436c73f27979dbc9e74f59d
Morgane Lailler <morgane.lailler.pro@gmail.com>
2021-11-15 13:27:13 +0100

    drop dead code

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -82,7 +82,6 @@ class QuestionController extends AbstractController
                 $answerId = $answerChecked[1];
             }
             if (isset($questionId) && isset($answerId)) {
-                //&& !in_array($questionId, $_SESSION["questionsWellAnswered"])
                 $answersManager = new AnswersManager();
                 //Fetch the answer and its properties
                 $chosenAnswer = $answersManager->selectAnswerFromChecked($answerId);
