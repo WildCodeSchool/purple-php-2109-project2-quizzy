3bf22f5e00440c91178e32ae08e1eb027a984b39
Fouzia <fouzia-asri_student2021@wilder.school.com>
2021-11-17 09:26:10 +0100

    Display Legalnotices

--- src/routes.php
+++ src/routes.php
@@ -8,4 +8,6 @@
 // e.g route '/item/edit?id=1' will execute $itemController->edit(1)
 return [
     '' => ['QuestionController', 'show',],
+    'report' => ['ErrorReportingController', 'addMessage',['id'],],
+    'legalnotices' => ['LegalNoticesController', 'legalNotices']
 ];
